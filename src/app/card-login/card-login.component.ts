0431eb19ed58b8869394532d5be35c2d5c89889c
CristianoAlberto <cristianoalberto908347@gmail.com>
2024-02-21 01:52:06 +0100

    feat: Fazendo tela login

---
+++ src/app/card-login/card-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-login',
+  templateUrl: './card-login.component.html',
+  styleUrl: './card-login.component.scss'
+})
+export class CardLoginComponent {
+
+}
